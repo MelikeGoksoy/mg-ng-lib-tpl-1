8daf34f71af32577addc8a74486d944da59a31aa
melike <melike.goksoy@cantekgroup.com>
2019-05-08 15:50:24 +0300

    stabil app

--- projects/apps/test-app/src/app/app.module.ts
+++ projects/apps/test-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+console.log("app comp" , AppComponent);
 import { HelloComponent } from './hello.component';
 
 @NgModule({
--- projects/apps/test-app/src/main.ts
+++ projects/apps/test-app/src/main.ts
@@ -2,9 +2,9 @@ import './polyfills';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+console.log("bidi");
 import { AppModule } from './app/app.module';
-
+console.log("appmodule",AppModule);
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
